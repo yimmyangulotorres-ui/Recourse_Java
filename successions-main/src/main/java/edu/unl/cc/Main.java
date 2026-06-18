8ac7e853ef1c6de99ec459bfabfc29f6dd832144
roxogamer11-spec <roxogamer11@gmail.com>
2026-06-18 10:46:12 -0500

    Update Main.java

--- successions-main/src/main/java/edu/unl/cc/Main.java
+++ successions-main/src/main/java/edu/unl/cc/Main.java
@@ -21,6 +21,7 @@ public class Main {
         System.out.println("2. Serie de primos elevados al cubo  hasta un limite (S = 1^3 + 3^3 + 5^3 + 7^3 + 11^3 + 13^3 ... + N^3): ");
         System.out.println("7. Serie de primos elevados a la raiz de numeros impares hasta un n términos (S = 1^(1/1) + 3^(1/3) + 5^(1/5) + 7^(1/7) + 11^(1/9) + 13^(1/11)): ");
         System.out.println("8. Serie de primos a la raíz cúbica hasta N términos (S = 1^(1/3) + 3^(1/3) + 5^(1/3) + 7^(1/3) + 11^(1/3) + 13^(1/3) ...): ");
+        System.out.println("9.Serie de primos elevados a la raiz cuadrada hasta un limite (S = 1^(1/2) + 3^(1/2) + 5^(1/2) + 7^(1/2) + 11^(1/2) + 13^(1/2)+ .. + N^(1/2):"):
         System.out.println("10. Serie de primos hasta un limite (S = 1 + 2 + 3 + 5 + 7 + 11 + 13 + .. + N: ");
     }
 
