6efc8eaa18c04ef854b543166f77f0dd8d9cc358
yimmyangulotorres-ui <yimmyangulotorres@gmail.com>
2026-06-18 21:36:06 -0500

    Clean up Main.java by removing comments
    Removed decompiler comments from Main.java

--- successions-main/src/main/java/edu/unl/cc/Main.java
+++ successions-main/src/main/java/edu/unl/cc/Main.java
@@ -1,8 +1,3 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by Fernflower decompiler)
-//
-
 package edu.unl.cc;
 
 import edu.unl.cc.succession.business.EvenNumberCalculatorUpToLimit;
