c4584083dcaea93542e01509de21c4509ddf9f67
yimmyangulotorres-ui <yimmyangulotorres@gmail.com>
2026-06-19 06:42:43 -0500

    Update PrimeNumberCubeRootCalculatorWithTerm.java

--- successions-main/src/main/java/edu/unl/cc/succession/business/PrimeNumberCubeRootCalculatorWithTerm.java
+++ successions-main/src/main/java/edu/unl/cc/succession/business/PrimeNumberCubeRootCalculatorWithTerm.java
@@ -5,7 +5,7 @@ import edu.unl.cc.succession.model.Successionable;
 public class PrimeNumberCubeRootCalculatorWithTerm implements Successionable, Printable {
 
 /**
- 8. Serie de primos elevados a la raiz cúbica hasta un n términos 
+ * 8. Serie de primos elevados a la raiz cúbica hasta un n términos 
  * (S = 1^(1/3) + 3^(1/3) + 5^(1/3) + 7^(1/3) + 11^(1/3) + 13^(1/3)):
  * @author wduck (Irvin Armijos)
  */
