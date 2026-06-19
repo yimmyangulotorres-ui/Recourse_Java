3260919a3cb894a04ce52bf33f147dfb7058327b
yimmyangulotorres-ui <yimmyangulotorres@gmail.com>
2026-06-18 21:35:51 -0500

    Update Main.java

--- successions-main/src/main/java/edu/unl/cc/Main.java
+++ successions-main/src/main/java/edu/unl/cc/Main.java
@@ -1,30 +1,55 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
 package edu.unl.cc;
 
-import edu.unl.cc.succession.business.*;
+import edu.unl.cc.succession.business.EvenNumberCalculatorUpToLimit;
+import edu.unl.cc.succession.business.PrimeNumberCalculatorUpToLimit;
+import edu.unl.cc.succession.business.PrimeNumberCubeRootCalculatorWithTerm;
+import edu.unl.cc.succession.business.PrimeNumberWithPowCalculatorWithTerm;
+import edu.unl.cc.succession.business.PrimeNumbersCubedSeriesUpToLimit;
+import edu.unl.cc.succession.business.PrimeNumbersCubedSeriesWithNTerms;
+import edu.unl.cc.succession.business.PrimeNumbersEvenPowerCalculatorUpToLimit;
+import edu.unl.cc.succession.business.PrimeNumbersSquareRootSeriesUpToLimit;
+import edu.unl.cc.succession.business.PrimeNumbersWithEvenRootSeriesUpToLimit;
+import edu.unl.cc.succession.business.PrimeNumbersWithOddPowersSeriesWithTerm;
 import edu.unl.cc.succession.model.Printable;
 import edu.unl.cc.succession.model.Successionable;
-
+import java.security.InvalidParameterException;
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-
-    private static void printMenu(){
-        System.out.println("CALCULADORA DE SERIES");
-        System.out.println("1. Serie de numeros pares hasta un limite (S = 2 + 4 + 6 + 8 + ... N): ");
-        System.out.println("2. Serie de primos elevados al cubo  hasta un limite (S = 1^3 + 3^3 + 5^3 + 7^3 + 11^3 + 13^3 ... + N^3): ");
-        System.out.println("7. Serie de primos elevados a la raiz de numeros impares hasta un n términos (S = 1^(1/1) + 3^(1/3) + 5^(1/5) + 7^(1/7) + 11^(1/9) + 13^(1/11)): ");
-        System.out.println("8. Serie de primos a la raíz cúbica hasta N términos (S = 1^(1/3) + 3^(1/3) + 5^(1/3) + 7^(1/3) + 11^(1/3) + 13^(1/3) ...): ");
-        System.out.println("10. Serie de primos hasta un limite (S = 1 + 2 + 3 + 5 + 7 + 11 + 13 + .. + N: ");
+    public Main() {
     }
 
-    private static int readOption(Scanner scanner){
-        int option;
+    private static void printMenu() {
+        System.out.println("\n╔═════════════════════════════════════════════════════════════════════════════════════════════════════════════════╗");
+        System.out.println("║                                            CALCULADORA DE SERIES                                                ║");
+        System.out.println("╠═════════════════════════════════════════════════════════════════════════════════════════════════════════════════╣");
+        System.out.println("║1. Serie de numeros pares hasta un limite (S = 2 + 4 + 6 + 8 + ... N):                                           ║");
+        System.out.println("║2. Serie de primos elevados al cubo  hasta un limite (S = 1^3 + 3^3 + 5^3 + 7^3 + 11^3 + 13^3 ... + N^3):        ║");
+        System.out.println("║3. Serie de primos elevados al cubo  hasta N términos (S = 1^3 + 3^3 + 5^3 + 7^3 + 11^3 + 13^3 ...)              ║");
+        System.out.println("║4. Serie de primos elevados por pares hasta un limite (S = 1^2 + 3^4 + 5^6 + 7^8 + 11^10 + 13^(12) ... + N):     ║");
+        System.out.println("║5. Serie de primos elevados a impares hasta n térmimos (S = S = 1^1 + 3^3 + 5^5 + 7^7 + 11^9 + 13^11 ..):        ║");
+        System.out.println("║6. Serie de primos elevados a la raiz de numeros pares hasta un limite (S = 1^(1/2) + 3^(1/4) + ... + N):        ║");
+        System.out.println("║7. Serie de primos elevados a la raiz de numeros impares hasta un n términos (S = 1^(1/1) + 3^(1/3) + 5^(1/5)):  ║");
+        System.out.println("║8. Serie de primos a la raíz cúbica hasta N términos (S = 1^(1/3) + 3^(1/3) + 5^(1/3) + 7^(1/3) + ...):          ║");
+        System.out.println("║9. Serie de primos elevados a la raiz cuadrada hasta un limite (S = 1^(1/2) + 3^(1/2) + 5^(1/2) + .... N^(1/2):  ║");
+        System.out.println("║10. Serie de primos hasta un limite (S = 1 + 2 + 3 + 5 + 7 + 11 + 13 + .. + N:                                   ║");
+        System.out.println("╠═════════════════════════════════════════════════════════════════════════════════════════════════════════════════╣");
+    }
+
+    private static int readOption(Scanner scanner) {
         printMenu();
-        System.out.print("Elija la opción de la serie que desea calcular: ");
-        option = scanner.nextInt();
-        return option;
+        System.out.print("║Elija la opción de la serie que desea calcular: ");
+        int option = scanner.nextInt();
+        if (option > 0 && option <= 10) {
+            return option;
+        } else {
+            throw new InvalidParameterException("The number is outside the allowed range (1 - 10)");
+        }
     }
 
     public static void main(String[] args) {
@@ -34,53 +59,26 @@ public class Main {
         int limit = scanner.nextInt();
         scanner.nextLine();
         Successionable serie = null;
-        switch (option){
-            case (1): {
-                serie = new EvenNumberCalculatorUpToLimit(limit);
-                break;
-            }
-            case (2):{
-                serie = new PrimeNumbersCubedSeriesUpToLimit(limit);
-                break;
-            }
-
-            case (4): {
-                serie = new PrimeNumbersEvenPowerCalculatorUpToLimit(limit);
-                break;
-            }
-            case (5):{
-                serie = new PrimeNumbersWithOddPowersSeriesWithTerm(limit);
-                break;
-            }
-            case (6): {
-                serie = new PrimeNumbersWithEvenRootSeriesUpToLimit(limit);
-                break;
-            }
-            case (7): {
-                serie = new PrimeNumberWithPowCalculatorWithTerm(limit);
-                break;
-            }
-            case (8): {
-                serie = new PrimeNumberCubeRootCalculatorWithTerm(limit);
-                break;
-            }
-            case (9): {
-                serie = new PrimeNumbersSquareRootSeriesUpToLimit(limit);
-                break;
-            }
-            case (10): {
-                serie = new PrimeNumberCalculatorUpToLimit(4,limit);
-                break;
-            }
-            default: {
-                System.out.println("Opción inválida");
-            }
-
+        switch (option) {
+            case 1 -> serie = new EvenNumberCalculatorUpToLimit(limit);
+            case 2 -> serie = new PrimeNumbersCubedSeriesUpToLimit(limit);
+            case 3 -> serie = new PrimeNumbersCubedSeriesWithNTerms(limit);
+            case 4 -> serie = new PrimeNumbersEvenPowerCalculatorUpToLimit(limit);
+            case 5 -> serie = new PrimeNumbersWithOddPowersSeriesWithTerm(limit);
+            case 6 -> serie = new PrimeNumbersWithEvenRootSeriesUpToLimit(limit);
+            case 7 -> serie = new PrimeNumberWithPowCalculatorWithTerm(limit);
+            case 8 -> serie = new PrimeNumberCubeRootCalculatorWithTerm(limit);
+            case 9 -> serie = new PrimeNumbersSquareRootSeriesUpToLimit(limit);
+            case 10 -> serie = new PrimeNumberCalculatorUpToLimit(limit);
+            default -> System.out.println("Opción inválida");
         }
-        if (serie != null){
+
+        if (serie != null) {
             Number result = serie.calculate();
             System.out.println(((Printable)serie).print());
-            System.out.println("S = " + result + "\n");
+            System.out.println("S = " + String.valueOf(result) + "\n");
         }
+
     }
 }
+
